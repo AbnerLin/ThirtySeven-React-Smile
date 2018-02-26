06fe143a35a9a01c3e5670ec3c0b87a8bcb8c8b5
AbnerLin <shaoyang0805@gmail.com>
2018-02-26 11:18:53 +0800

    test drag on stop event handle.

--- src/Map/Furnish/index.js
+++ src/Map/Furnish/index.js
@@ -5,40 +5,41 @@ import './index.css';
 
 class Furnish extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+    this.furnishOnClick = this.furnishOnClick.bind(this);
+    this.onDragStop = this.onDragStop.bind(this);
+  }
 
-        this.furnishOnClick = this.furnishOnClick.bind(this);
-        this.onDragStop = this.onDragStop.bind(this);
-    }
+  furnishOnClick(furnishId) {
+    console.log(furnishId + ' on click!'); //TODO
+  }
 
-    furnishOnClick(furnishId) {
-        console.log(furnishId + ' on click!'); //TODO
-    }
+  onDragStop(event, draggableData, furnishId) {
+      console.log(draggableData.x, draggableData.y);
+      console.log(furnishId);
+      // Update data.
+  }
 
-    onDragStop(event, draggableData) {
-        console.log('!!???');
-        console.log(draggableData);
-        console.log(event.target); //TODO
-    }
-
-    render() {
-       const furnish = this.props.furnish;
-       return (
-            <Draggable
-                bounds="parent"
-                axis="both"
-                handle=".handle"
-                onStop={this.onDragStop}
-                id={furnish.furnishid}
-                defaultPosition={{x: furnish.x, y: furnish.y}}>
-                <div className="box">
-                  { this.props.control ? <ToolBar furnish={furnish} /> : null }
-                  <div className="furnish d-flex justify-content-center align-items-center" onClick={() => this.furnishOnClick(furnish.furnishid)}>{furnish.name}</div>
-                </div>
-            </Draggable>
-        );
-    }
+  render() {
+     const furnish = this.props.furnish;
+     return (
+          <Draggable
+              bounds="parent"
+              axis="both"
+              handle=".handle"
+              onStop={ (event, draggableData) => {
+                this.onDragStop(event, draggableData, furnish.furnishid);
+              }}
+              id={furnish.furnishid}
+              defaultPosition={{x: furnish.x, y: furnish.y}}>
+              <div className="box">
+                { this.props.control ? <ToolBar furnish={furnish} /> : null }
+                <div className="furnish d-flex justify-content-center align-items-center" onClick={() => this.furnishOnClick(furnish.furnishid)}>{furnish.name}</div>
+              </div>
+          </Draggable>
+      );
+  }
 }
 
 export default Furnish;
