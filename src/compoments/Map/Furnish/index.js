6ff9a3725dedfe3b1ddbd59f18cd051ebefbac82
AbnerLin <shaoyang0805@gmail.com>
2018-04-30 16:27:40 +0800

    Modify component structure.

--- src/compoments/Map/Furnish/index.js
+++ src/compoments/Map/Furnish/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Draggable from 'react-draggable';
 import PropTypes from 'prop-types';
-import ToolBar from '../ToolBar';
+import ToolBar from './ToolBar';
 import './index.css';
 import ThirtySeven from '../../../common-utils/ThirtySeven.js';
 import alertify from 'alertify.js';
